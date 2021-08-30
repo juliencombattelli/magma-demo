17503f830b1eb40fd47aa9ad4d1d1202dabdadf2
Julien Combattelli <julien.combattelli@gmail.com>
2021-08-30 22:10:49 +0200

    Move device checks into magma

--- src/main.cpp
+++ src/main.cpp
@@ -51,86 +51,8 @@ int main(int argc, char* argv[]) try {
     magma::GlfwWindow::Ptr window = magma::GlfwWindow::create(
         default_window_width, default_window_height, applicationName, nullptr, nullptr);
 
-    vk::raii::PhysicalDevice phy = instance.pickPhysicalDevice();
-
     vk::raii::SurfaceKHR surface = instance.makeSurface(window.get());
-
-    const std::vector<const char*> device_extensions = { VK_KHR_SWAPCHAIN_EXTENSION_NAME };
-
-    vk::raii::PhysicalDevices physical_devices(instance);
-    vk::raii::PhysicalDevice physical_device(instance, nullptr);
-    for (auto&& phydevice : physical_devices) {
-        const auto& device_properties = phydevice.getProperties();
-        std::cout << "Physical device:\n"
-                  << "\tname: " << device_properties.deviceName << "\n"
-                  << "\ttype: " << vk::to_string(device_properties.deviceType) << "\n";
-
-        // Get all available device extensions
-        auto available_device_extensions = phydevice.enumerateDeviceExtensionProperties();
-        std::set<std::string> available_device_extension_set;
-        for (const auto& extensions : available_device_extensions) {
-            available_device_extension_set.insert(extensions.extensionName);
-        }
-
-        // Get required device extensions
-        std::set<std::string> required_device_extension_set(
-            device_extensions.begin(), device_extensions.end());
-
-        // Assert no required device extension is missing
-        std::set<std::string> missing_device_extension_set;
-        std::ranges::set_difference(required_device_extension_set, available_device_extension_set,
-            std::inserter(missing_device_extension_set, missing_device_extension_set.end()));
-        if (!missing_device_extension_set.empty()) {
-            for (const auto& missing : missing_device_extension_set) {
-                std::cerr << "Missing device extension: " << missing << "\n";
-            }
-            continue;
-        }
-
-        // Assert at least on surface format is provided by the device
-        auto surface_formats = phydevice.getSurfaceFormatsKHR(*surface);
-        if (surface_formats.empty()) {
-            std::cerr << "Device does not provide any surface format\n";
-            continue;
-        }
-
-        // Assert at least on surface present mode is provided by the device
-        auto surface_present_modes = phydevice.getSurfacePresentModesKHR(*surface);
-        if (surface_present_modes.empty()) {
-            std::cerr << "Device does not provide any surface present mode\n";
-            continue;
-        }
-
-        // Assert device has graphics and presentation capabilities
-        auto queue_family_properties = phydevice.getQueueFamilyProperties();
-        std::optional<uint32_t> queue_graphics_family_index;
-        for (auto queue_family_it = queue_family_properties.begin();
-             queue_family_it != queue_family_properties.end(); ++queue_family_it) {
-            if (queue_family_it->queueFlags & vk::QueueFlagBits::eGraphics) {
-                queue_graphics_family_index
-                    = std::distance(queue_family_properties.begin(), queue_family_it);
-            }
-            if (!queue_graphics_family_index) {
-                std::cerr << "Device does not support graphics\n";
-                continue;
-            }
-            auto present_support
-                = phydevice.getSurfaceSupportKHR(*queue_graphics_family_index, *surface);
-            if (!present_support) {
-                std::cerr << "Device does not support presentation\n";
-                continue;
-            }
-        }
-
-        // TODO compute score based on device performance (type and memory available)
-        //      and choose the highest ranked device
-        physical_device = std::move(phydevice);
-        break;
-    }
-
-    if (!*physical_device) {
-        throw std::runtime_error("No suitable GPU found");
-    }
+    vk::raii::PhysicalDevice physical_device = instance.pickPhysicalDevice(surface);
 
     uint32_t graphicsQueueFamilyIndex
         = findGraphicsQueueFamilyIndex(physical_device.getQueueFamilyProperties());
@@ -143,6 +65,8 @@ int main(int argc, char* argv[]) try {
     };
 
     vk::PhysicalDeviceFeatures device_features{};
+
+    const std::vector<const char*> device_extensions = { VK_KHR_SWAPCHAIN_EXTENSION_NAME };
 
     vk::DeviceCreateInfo device_create_info{
         .queueCreateInfoCount = 1,
